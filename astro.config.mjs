6cfbbc28b0b91fb8c4cc8589881a6ceeb612c8bc
Your Name <your.email@example.com>
2026-04-13 15:22:43 +0300

    Add Netlify adapter for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import netlify from '@astrojs/netlify';
+
 
 export default defineConfig({
   integrations: [react()],
+    output: 'server',
+  adapter: netlify(),
   vite: {
     plugins: [tailwindcss()],
   },
